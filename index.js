4f238219096a885fbda9a3ccc8325dc632c7ba3f
guoshuai <406601626@qq.com>
2021-11-22 15:11:36 +0800

    Default Changelist

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// console.log('this is gs npm repository test project')
+console.log('this is gs npm repository test project:',process.env.npm_config_testarg)
 exports.sayHello = function () {
   return 'this is gs npm repository test project'
 }
